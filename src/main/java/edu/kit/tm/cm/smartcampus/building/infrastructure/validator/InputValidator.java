bddb76aa2fcedff0b13cee26fd55061d72838a30
Johannes von Geisau <johannes.von.geisau@t-online.de>
2022-07-19 19:27:05 +0200

    add Input Validator

---
+++ src/main/java/edu/kit/tm/cm/smartcampus/building/infrastructure/validator/InputValidator.java
@@ -0,0 +1,90 @@
+package edu.kit.tm.cm.smartcampus.building.infrastructure.validator;
+
+import edu.kit.tm.cm.smartcampus.building.infrastructure.exceptions.InvalidArgumentsException;
+import lombok.AllArgsConstructor;
+import org.springframework.data.util.Pair;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * Class representing an input validator which checks given inputs and thereby validates them and throws the right
+ * exceptions when an input is invalid.
+ */
+@Component
+@AllArgsConstructor
+public class InputValidator {
+
+  /**
+   * Validates weather objects are not null or not.
+   *
+   * @param objects Map of objects to be checked and their names (key=name, value=object)
+   */
+  public void validateNotNull(Map<String, Object> objects) {
+    InvalidArgumentsException invalidArgumentsException = new InvalidArgumentsException();
+    boolean valid = true;
+
+    for (Map.Entry<String, Object> entry : objects.entrySet()) {
+      if (entry.getValue() == null) {
+        invalidArgumentsException.appendWrongArguments(
+                entry.getKey(), "null", "should not be null", true);
+        valid = false;
+      }
+    }
+
+    if (!valid) {
+      throw invalidArgumentsException;
+    }
+  }
+
+  /**
+   * Validates weather Strings are not empty or not.
+   *
+   * @param strings Map of strings to be checked and their names (key=name, value=string)
+   */
+  public void validateNotEmpty(Map<String, String> strings) {
+    InvalidArgumentsException invalidArgumentsException = new InvalidArgumentsException();
+    boolean valid = true;
+
+    for (Map.Entry<String, String> entry : strings.entrySet()) {
+      if (!entry.getValue().isEmpty()) {
+        invalidArgumentsException.appendWrongArguments(
+                entry.getKey() + ": ", entry.getValue(), "should not be empty", true);
+        valid = false;
+      }
+    }
+
+    if (!valid) {
+      throw invalidArgumentsException;
+    }
+  }
+
+  /**
+   * Validates weather Strings match given regexes or not.
+   *
+   * @param strings Map of strings and their regexes to be checked and their names (key=name,
+   *                value=pair of string and regex)
+   */
+  public void validateMatchesRegex(Map<String, Pair<String, String>> strings) {
+    InvalidArgumentsException invalidArgumentsException = new InvalidArgumentsException();
+    boolean valid = true;
+
+    for (Map.Entry<String, Pair<String, String>> entry : strings.entrySet()) {
+      if (!entry.getValue().getFirst().matches(entry.getValue().getSecond())) {
+        invalidArgumentsException.appendWrongArguments(
+                entry.getKey(),
+                entry.getValue().getFirst(),
+                "should match: " + entry.getValue().getSecond(),
+                true);
+        valid = false;
+      }
+    }
+
+    if (!valid) {
+      throw invalidArgumentsException;
+    }
+  }
+
+  //TODO location validation + im service aufrufen
+
+}
