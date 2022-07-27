86819ba8781a9bcb8754e03c12da8db8be87128f
Jonathan Kramer <jonathan.kramer21@gmail.com>
2022-07-27 10:30:18 +0200

    JavaDoc

--- src/main/java/edu/kit/tm/cm/smartcampus/building/logic/LogicUtils.java
+++ src/main/java/edu/kit/tm/cm/smartcampus/building/logic/LogicUtils.java
@@ -8,11 +8,20 @@ import edu.kit.tm.cm.smartcampus.building.logic.model.Building;
 import edu.kit.tm.cm.smartcampus.building.logic.model.Component;
 import edu.kit.tm.cm.smartcampus.building.logic.model.Notification;
 import edu.kit.tm.cm.smartcampus.building.logic.model.Room;
-//TODO javadoc (überall!^^)
+
+/**
+ * This class provides utilities to work within the building domain.
+ */
 public class LogicUtils {
 
   private LogicUtils(){}
-  
+
+  /**
+   * Converts a BuildingRequest to a Building and sets all attributes of the Building to the values of the existing
+   * attributes of the Request
+   * @param buildingRequest the request to be converted
+   * @return the resulting Building
+   */
   public static Building convertBuildingRequestToBuilding(BuildingRequest buildingRequest) {
     Building building = new Building();
     building.setName(buildingRequest.getName());
@@ -23,6 +32,12 @@ public class LogicUtils {
     return building;
   }
 
+  /**
+   * Converts a RoomRequest to a Room and sets all attributes of the Room to the values of the existing
+   * attributes of the Request
+   * @param roomRequest the Request to be converted
+   * @return the resulting Room
+   */
   public static Room convertRoomRequestToRoom(RoomRequest roomRequest) {
     Room room = new Room();
     room.setName(roomRequest.getName());
@@ -34,6 +49,12 @@ public class LogicUtils {
     return room;
   }
 
+  /**
+   * Converts a ComponentRequest to a Component and sets all attributes of the Component to the values of the existing
+   * attributes of the Request
+   * @param componentRequest the Request to be converted
+   * @return the resulting Component
+   */
   public static Component convertComponentRequestToComponent(ComponentRequest componentRequest) {
     Component component = new Component();
     component.setDescription(componentRequest.getDescription());
@@ -43,6 +64,12 @@ public class LogicUtils {
     return component;
   }
 
+  /**
+   * Converts a NotificationRequest to a Notification and sets all attributes of the Notification to the values of the
+   * existing attributes of the Request
+   * @param notificationRequest the Request to be converted
+   * @return the resulting Notification
+   */
   public static Notification convertNotificationRequestToNotification(NotificationRequest notificationRequest) {
     Notification notification = new Notification();
     notification.setDescription(notificationRequest.getDescription());
