1f8e6dd9e73a5cea10d60ee10e1803410d3038f5
Johannes von Geisau <johannes.von.geisau@t-online.de>
2022-08-12 09:00:37 +0200

    cosmetics v2

---
+++ src/test/java/edu/kit/tm/cm/smartcampus/building/api/controller/room/RoomControllerTest.java
@@ -0,0 +1,8 @@
+package edu.kit.tm.cm.smartcampus.building.api.controller.room;
+
+public class RoomControllerTest {
+
+  //TODO (@Jonathan) "copy paste vom Building controller test" und das für diese klasse und für
+  // component und notification
+
+}
