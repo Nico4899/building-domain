1a509a0d6bc55640f245687537745ee4fffa120a
Johannes von Geisau <johannes.von.geisau@t-online.de>
2022-09-27 12:44:51 +0200

    description as text

--- database/notification.sql
+++ database/notification.sql
@@ -3,7 +3,7 @@ create table notification
     identification_number        varchar(255) not null
         primary key,
     creation_time                timestamp,
-    description                  varchar(255),
+    description                  text,
     last_modified_time           timestamp,
     parent_identification_number varchar(255),
     title                        varchar(255)
